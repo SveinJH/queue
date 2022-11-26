4b33a0c8ea54206a139c40d3e8e5cf019d91374a
Svein Jakob Høie <sveinjakob@hoie.dev>
2022-11-26 17:01:09 +0100

    add prod client id

--- app/utils/config.server.ts
+++ app/utils/config.server.ts
@@ -17,7 +17,7 @@ export const envConfig: EnvironmentConfig = {
         redirect_uri: '',
     },
     production: {
-        client_id: '',
-        redirect_uri: '',
+        client_id: 'ad9f8f52580b4b7a957b78691d1b2215',
+        redirect_uri: 'https://queue-ten.vercel.app/callback',
     },
 };
